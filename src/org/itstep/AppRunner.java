d379edb0e6be595d8d7eee2ad19def3782f30c1d
student <student@K04-02.lan.itstep.kiev.ua>
2018-05-15 21:00:15 +0300

    Add test files

--- src/org/itstep/AppRunner.java
+++ src/org/itstep/AppRunner.java
@@ -1,12 +1,11 @@
 package org.itstep;
 
-public class AppRunner {
+public class AppRunner extends Object {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Name firstName = new Name();
-		String myName = firstName.getName("alexey");
+		String myName = firstName.getName("Alexey Sensey");
 	System.out.println(myName);
-	}
-
 }
+} 
